c53fe8f3ad4ce3ceb7f9489a272540b047f9972e
lohainyyy <128819915+lohainyyy@users.noreply.github.com>
2024-07-05 08:03:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function comprar() {
+const width= 800;
+const height= 600;
+const left= (screen.width / 2)- (width / 2);
+const top= (screen.heigth / 2)- (height / 2);
+window.open ('cadastro.html' , 'pagamento' , 'width=${width},height=${height}, top=${top}, left=${left}');
+
+
+
+
+
+}
